fddc8aa5d652a2596da18faa073b7b4fadb9dd28
Athu1-krishna <athul.krishna.0018@gmail.com>
2024-12-15 19:12:20 +0530

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('./config/connection')
 pfServer.use(cors())
 pfServer.use(express.json())
 pfServer.use(router)
-
+pfServer.use('/uploads', express.static('./uploads'))
 const PORT = 3000 || process.env.PORT
 
 pfServer.listen(PORT, ()=>{
